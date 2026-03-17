59a2d22cda51503ce04c8401c7ca0fa044a3d1e2
Alberto Galeano <all.galeano.diaz@gmail.com>
2026-03-17 15:18:00 +0100

    CreateUser method created in User repository

--- internal/store/users.go
+++ internal/store/users.go
@@ -17,6 +17,24 @@ type UserStore struct {
 	db *sql.DB
 }
 
-func (s *UserStore) Create(ctx context.Context) error {
+func (s *UserStore) Create(ctx context.Context, user *User) error {
+	query := `
+		INSERT INTO users (username, password, email) VALUES ($1, $2, $3)
+		RETURNING id, created_at
+	`
+	err := s.db.QueryRowContext(
+		ctx,
+		query,
+		user.Username,
+		user.Password,
+		user.Email,
+	).Scan(
+		&user.ID,
+		&user.CreatedAt,
+	)
+
+	if err != nil {
+		return err
+	}
 	return nil
 }

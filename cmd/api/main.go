a9e97a0a54582f1d7a99827f800d21c1c7706820
Alberto Galeano <all.galeano.diaz@gmail.com>
2026-03-17 15:24:43 +0100

    Database configuration

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,12 @@ import (
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr: env.GetString("DB_ADDR", "postgres://admin:admin@localhost/social?sslmode=disabled"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime: env.GetString("DB_MAX_IDLE_TIME", "15min"),
+		},
 	}
 
 	store := store.NewStorage(nil)
